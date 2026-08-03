959c302ec87f3dcc7384d330259a838e3620eeb6
Abhishek_Adhau <abhishekadhau84@gmail.com>
2026-08-03 22:41:02 +0530

    Time: 517 ms (58.45%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,12 @@
+/* Write your PL/SQL query statement below */
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(*) = (
+    SELECT MAX(cnt)
+    FROM (
+        SELECT COUNT(*) AS cnt
+        FROM Orders
+        GROUP BY customer_number
+    )
+);
