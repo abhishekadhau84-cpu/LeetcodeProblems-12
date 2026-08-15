835ea1b5f63d5dd79c2712624c867bf031c1dd45
Abhishek_Adhau <abhishekadhau84@gmail.com>
2026-08-15 11:28:03 +0530

    Time: 826 ms (5.38%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,2 +1,3 @@
 /* Write your PL/SQL query statement below */
-select max(num) as num from MyNumbers where num IN (select num from MyNumbers group by num Having count(*)=1 );
+select max(num) as num from MyNumbers 
+where num IN (select num from MyNumbers group by num Having count(*)=1 );
